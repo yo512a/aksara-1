9e1298e359b6e4d7a4ef52e25844c740f5f4603c
Raka Audira Fauzi <rakaaudira@gmail.com>
2019-12-12 00:04:38 +0700

    Add files via upload

---
+++ application/views/Template/footer.php
@@ -0,0 +1,36 @@
+<div id='footer' class='fl-w100'>
+<div class='container'>
+
+<div class='col col-l'>
+<div class='widget sosmed'>
+<a href='#' class='d-flex ai-c sm-fb' title='Facebook'><i class='fab fa-facebook-f'></i>Facebook</a>
+<a href='#' class='d-flex ai-c sm-tw' title='Twitter'><i class='fab fa-twitter'></i>Twitter</a>
+<a href='#' class='d-flex ai-c sm-ig' title='Facebook'><i class='fab fa-instagram'></i>Instagram</a>
+</div>
+</div>
+
+<div class='col col-r'>
+<div class='widget fs-16'>
+The best stock books & creatives to explore your knowledge. Explore 100+ free books to read and a lot creativity
+</div>
+<div class='widget menus'>
+<a href='#' title='Buku'>Buku</a>
+<a href='#' title='Blog'>Blog</a>
+<a href='#' title='Tentang Kami'>Tentang Kami</a>
+<a href='#' title='Kebijakan Privasi'>Kebijakan Privasi</a>
+</div>
+</div>
+
+<div class='col col-100 fl-w100'>
+<form method='post' action='' class='widget subscribe fl-w100'>
+<input class='tf' type='text' placeholder='Email address...'>
+<button type='submit' class='tf submit'>Subscribe</button>
+
+</form>
+</div>
+
+</div>
+<div class='credit fl-w100'>
+Copyright &copy; <script>document.write(new Date().getFullYear())</script> - Aksara
+</div>
+</div>
